3e51b277fafe5cd6d35a7dad692feb9ae231b989
Kevin Schoell <schoell411@gmail.com>
2019-10-16 03:15:16 -0500

    completed mvp

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -5,12 +5,12 @@ const Movie = (props) => {
   const [movie, setMovie] = useState({});
  
   useEffect(() => {
-    const id = 1;
+    const id = Number(props.match.params.id);
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
        axios
-        .get(`http://localhost:5000/api/movies/${movie.id}`)
+        .get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
           setMovie(response.data);
         })
@@ -18,7 +18,7 @@ const Movie = (props) => {
           console.error(error);
         });
 
-  },[]);
+  },[props.match.params.id]);
   
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
@@ -43,7 +43,8 @@ const Movie = (props) => {
         </div>
         <h3>Actors</h3>
 
-        {stars.map(star => (
+        {stars &&
+        stars.map(star => (
           <div key={star} className="movie-star">
             {star}
           </div>
--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,35 @@
 import React from 'react';
 
 const MovieCard = props => {
-  return;
-};
+  if(!props){
+    return 
+    <div>
+      Loading movie information..
+    </div>;
+    };
+  const { title, director, metascore, starts} = props;
+  return (
+    <div className="save-wrapper">
+      <div className="movie-card">
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
+        <h3>Actors</h3>
+
+        {stars &&
+        stars.map(star => (
+          <div key={star} className="movie-star">
+            {star}
+          </div>
+        ))}
+      </div>
+      <div className="save-button">Save</div>
+    </div>
+  );
+}
 
 export default MovieCard;
